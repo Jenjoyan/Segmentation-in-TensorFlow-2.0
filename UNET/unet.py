12bc28b59edbbfa91a6ebf087f9bad68046c8227
Jen Joyan <jenjoyan919@gmail.com>
2025-07-17 12:06:57 +0530

    model update

--- UNET/unet.py
+++ UNET/unet.py
@@ -40,15 +40,15 @@ def build_unet(input_shape):
     s1, p1 = encoder_block(inputs, 64)
     s2, p2 = encoder_block(p1, 128)
     s3, p3 = encoder_block(p2, 256)
-    s4, p4 = encoder_block(p3, 512)
+    # s4, p4 = encoder_block(p3, 512)
 
     # print(s1.shape, s2.shape, s3.shape, s4.shape)
     # print(p1.shape, p2.shape, p3.shape, p4.shape)
 
-    b1 = conv_block(p4, 1024)
+    b1 = conv_block(p3, 512)
 
-    d1 = decoder_block(b1, s4, 512)
-    d2 = decoder_block(d1, s3, 256)
+    # d1 = decoder_block(b1, s4, 512)
+    d2 = decoder_block(b1, s3, 256)
     d3 = decoder_block(d2, s2, 128)
     d4 = decoder_block(d3, s1, 64)
 
